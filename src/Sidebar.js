600de85598e43129597e4c6734750d98d3d86768
ashwin <achidana@purdue.edu>
2018-05-22 18:26:42 -0400

    Sidebar completed

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,13 +3,16 @@ import quill from './quill.svg'
 import newIcon from './new.png'
 import newHover from './new-hover.png'
 
+import './Sidebar.css'
+
+
 
 
 const Sidebar = () => {
     return(
-        <div className="Sidebar">
-            <div className="logo">
-            <img src={quill} alt="Noteherder" />
+        <div className="Sidebar" >
+            <div className="logo" >
+            <img src={quill} alt="Noteherder"  />
             </div>
 
             <a href="/notes">
@@ -27,5 +30,19 @@ const Sidebar = () => {
     )
 }
 
+// const styles = {
+//     sidebar:{
+//         width:'6rem',
+//         backgroundColor:'#f3f3f3',
+//         padding:'0.5rem 0',
+//         display:'flex',
+//         flexDirection:'column',
+//         alignItems:'center'
+//     },
+//     logo:{
+
+//     }
+
+// }
 
 export default Sidebar
