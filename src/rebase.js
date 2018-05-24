11298263c40274f9090a927021c332bba3457f2c
ashwin <achidana@purdue.edu>
2018-05-24 10:36:30 -0400

    minor changes

--- src/rebase.js
+++ src/rebase.js
@@ -2,7 +2,7 @@ import Rebase from 're-base';
 import firebase from 'firebase/app';
 import 'firebase/database';
 
-var app = firebase.initializeApp({
+const app = firebase.initializeApp({
     apiKey: "AIzaSyDxePri8QOUvm2hMbGCKjLeyG7rI-q90UI",
     authDomain: "noteherder-dd790.firebaseapp.com",
     databaseURL: "https://noteherder-dd790.firebaseio.com",
@@ -11,7 +11,7 @@ var app = firebase.initializeApp({
     messagingSenderId: "875679986580"
 });
 
-var db = firebase.database(app);
-var base = Rebase.createClass(db);
+const db = firebase.database(app);
+const base = Rebase.createClass(db);
 
 export default base;
